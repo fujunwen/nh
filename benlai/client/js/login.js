65569c08be92658a7548acdf9f1b4fcf4892c206
zs <123456@qq.com>
2020-06-30 14:08:28 +0800

    登陆注册2

--- benlai/client/js/login.js
+++ benlai/client/js/login.js
@@ -1,21 +1,21 @@
 $(() => {
+    $("#rember").click(function () {
+        let rember = $("#rember").is(":checked");
+        if (rember) {
+            $(".warn").css("display","block");
+        } else {
+            $(".warn").css("display","none");
+        }
 
-    $(".tab-login-item").click(function() {
-        /* (1) 设置当前标签的选中状态 */
-        $(this).addClass("active").siblings().removeClass("active");
-        /* (2) 切换显示对应的内容 */
-
-        let index = $(this).index();
-        $(".loginView").eq(index).addClass("loginViewCurrent").siblings().removeClass("loginViewCurrent");
     })
-
     /* 获取登录按钮，添加事件 */
-    $("#loginBtn").click(function() {
-        let username = $.trim($("#username-ID").val());
+    $("#btn").click(function () {
+        let phone = $.trim($("#phone-ID").val());
+        console.log(phone);
         let password = $.trim($("#password-ID").val());
 
         /* 先检查用户名和密码和是否勾选，都满足则发请求 */
-        if (username.length == 0) {
+        if (phone.length == 0) {
             alert("用户名不能为空");
             return
         }
@@ -25,22 +25,23 @@ $(() => {
             return;
         }
 
-        if (!$("#protocol").is(":checked")) {
+        /*if (!$("#protocol").is(":checked")) {
             alert("请阅读并同意用户协议");
             return;
-        }
+        }*/
 
         $.ajax({
             type: "post",
             url: "../../server/login.php",
             dataType: "json",
-            data: `username=${username}&password=${md5(password).slice(0,15)}`
+            data: `phone=${phone}&password=${md5(password).slice(0, 15)}`
         }).done(data => {
             // alert(data.msg);
             /* 如果 */
             if (data.status == "success") {
                 alert(data.msg);
                 /* 跳转 */
+                location.href = "../html/index.html";
             } else {
                 alert(data.msg);
             }
